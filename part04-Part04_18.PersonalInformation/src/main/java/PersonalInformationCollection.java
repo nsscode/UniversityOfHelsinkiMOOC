454ed855efa0f41a881ce82d335d453259f080e6
unknown <nssteenbeek@gmail.com>
2024-03-07 10:37:51 +0100

    finished part 04_18_PersonalInformation

--- part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -2,13 +2,43 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.sound.midi.MidiDevice.Info;
+
 public class PersonalInformationCollection {
 
     public static void main(String[] args) {
-        // implement here your program that uses the PersonalInformation class
 
+        // Create collection to store PersonalInformation objects
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        // Keep promting user for information until they enter an empty first name
+        while (true) {
+            // Ask user for their first name
+            System.out.print("First name: ");
+            String firstName = scanner.nextLine();
 
+            // If first name is empty, break out of the loop
+            if (firstName.isEmpty()) {
+                break;
+            }
+
+            // Ask user for their last name
+            System.out.print("Last name: ");
+            String lastName = scanner.nextLine();
+
+            // Ask user for their identification number
+            System.out.print("Identification number: ");
+            String identificationNumber = scanner.nextLine();
+
+            // Create a new PersonalInformation object and add it to the collection
+            infoCollection.add(new PersonalInformation(firstName, lastName, identificationNumber));
+        }
+
+        System.out.println(" ");
+        // Print out first and last name of each person in the collection
+        for (PersonalInformation person : infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+
+        }
     }
 }
