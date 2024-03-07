e557cef7bc3b5aea10fabb3d86b11b44420176a2
unknown <nssteenbeek@gmail.com>
2024-03-07 09:58:24 +0100

    finished part04_17_Items

--- part04-Part04_17.Items/src/main/java/Items.java
+++ part04-Part04_17.Items/src/main/java/Items.java
@@ -5,10 +5,23 @@ import java.util.Scanner;
 public class Items {
 
     public static void main(String[] args) {
-        // implement here your program that uses the class Item
 
         ArrayList<Item> items = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.print("Name: ");
+            String nameItem = scanner.nextLine();
+            if (nameItem.isEmpty()) {
+                break;
+            }
+            // Add to the list a new item
+            // whose name is the previous user input
+            items.add(new Item(nameItem));
+            }
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        }
     }
-}
+
