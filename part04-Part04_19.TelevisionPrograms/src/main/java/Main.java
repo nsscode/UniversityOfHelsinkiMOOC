f3531c0b651b058375da999ba87ac3dbe59b7f4e
unknown <nssteenbeek@gmail.com>
2024-03-08 16:51:45 +0100

    finished part04_19_TelevisionPrograms

--- part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -9,5 +9,38 @@ public class Main {
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        // Keep promting the user until they return an empty name
+        while (true) {
+            // Ask user for the name of the show
+            System.out.print("Name: ");
+            String nameShow = scanner.nextLine();
+
+            if (nameShow.isEmpty()) {
+                break;
+            }
+
+            // Ask user for the duration of the show
+            System.out.print("Duration: ");
+            // int durationShow = scanner.nextInt();
+            int durationShow = Integer.valueOf(scanner.nextLine());
+
+            // Create a TelevionProgram object and add it to the collection
+            programs.add(new TelevisionProgram(nameShow, durationShow));
+
+        }
+
+        System.out.println(" ");
+
+        // Ask the user for the maximum duration of the show
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = scanner.nextInt();
+
+        // Print out all the shows that are smaller or equal to the max duration
+        for (TelevisionProgram televisionProgram : programs) {
+            if (televisionProgram.getDuration() <= maxDuration) {
+                System.out.println(televisionProgram);
+            }
+
+        }
     }
 }
