d0041e32590700ba242664993842e4af0526797a
unknown <nssteenbeek@gmail.com>
2024-03-12 09:42:37 +0100

    finished part 04_25_PrintingASpecifiedFile

--- part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
+++ part04-Part04_25.PrintingASpecifiedFile/src/main/java/PrintingASpecifiedFile.java
@@ -7,5 +7,22 @@ public class PrintingASpecifiedFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Ask the user which file should be printed
+        System.out.println("Which file should have its contents printed?");
+        String input = scanner.nextLine();
+
+        // Search for the file name the user inputted
+        try (Scanner scanner2 = new Scanner(Paths.get(input))) {
+            // Print each line in the file
+            while (scanner2.hasNextLine()) {
+                String line = scanner2.nextLine();
+                System.out.println(line);
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.err.println("File not found" + e.getMessage());
+        }
+
     }
 }
