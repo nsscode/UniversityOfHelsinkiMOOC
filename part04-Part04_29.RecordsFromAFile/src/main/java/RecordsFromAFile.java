0781c9d4855f62f4e1f41e0895ce4ab6f8d793fb
unknown <nssteenbeek@gmail.com>
2024-03-14 10:18:11 +0100

    finished part04_29_RecordsFromAFile

--- part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
+++ part04-Part04_29.RecordsFromAFile/src/main/java/RecordsFromAFile.java
@@ -7,5 +7,33 @@ public class RecordsFromAFile {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Ask the user for the name of the file
+        System.out.println("Name of the file: ");
+        String file = scanner.nextLine();
+
+        // Search for the specified file
+        try (Scanner scanner2 = new Scanner(Paths.get(file))) {
+            while (scanner2.hasNextLine()) {
+                String line = scanner2.nextLine();
+
+                // Split the next line after each ,
+                // Save in an array
+                String[] parts = line.split(",");
+
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+
+                // Print plural of year if age is anything but 1
+                if (age != 1) {
+                    System.out.println(name + ", " + "age: " + age + " years");
+                } else {
+                    System.out.println(name + ", " + "age: " + age + " year");
+                }
+
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
     }
 }
