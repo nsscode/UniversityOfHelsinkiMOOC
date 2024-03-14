70df56be5b19e0c5bfad482c9cf91b01f9580f9f
unknown <nssteenbeek@gmail.com>
2024-03-14 09:39:09 +0100

    finished part04_28_NumbersFromAFile

--- part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -6,13 +6,35 @@ public class NumbersFromAFile {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numbers = 0;
 
+        // Ask user for what file
         System.out.print("File? ");
         String file = scanner.nextLine();
+
+        // Ask the user the lower bound
         System.out.print("Lower bound? ");
         int lowerBound = Integer.valueOf(scanner.nextLine());
+
+        // Ask the user the upper bound
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
+
+        // Search for the specified file
+        try (Scanner scanner2 = new Scanner(Paths.get(file))) {
+            while (scanner2.hasNextLine()) {
+                // Save the next line as an integer
+                int line = Integer.valueOf(scanner2.nextLine());
+                if (line >= lowerBound && line <= upperBound) {
+                    numbers++;
+                }
+            }
+        } catch (Exception e) {
+
+        }
+        
+        // Print the numbers
+        System.out.println("Numbers: " + numbers);
 
     }
 
