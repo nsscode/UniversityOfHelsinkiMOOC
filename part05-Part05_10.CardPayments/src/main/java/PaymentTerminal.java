8b8a3b2f8fe1d2f8eacf3c1414fa8425e4c4c23d
unknown <nssteenbeek@gmail.com>
2024-03-27 10:50:39 +0100

    finished part05_10_CardPayments

---
+++ part05-Part05_10.CardPayments/src/main/java/PaymentTerminal.java
@@ -0,0 +1,61 @@
+
+public class PaymentTerminal {
+
+    private double money; // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals; // number of sold hearty meals
+
+    public PaymentTerminal() {
+        this.money = 1000;
+    }
+
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            card.addMoney(sum);
+            this.money = this.money + sum;
+        }
+    }
+
+    public double eatAffordably(double payment) {
+        if (payment >= 2.50) {
+            this.money = this.money + 2.50;
+            this.affordableMeals++;
+            return payment - 2.50;
+
+        }
+        return payment;
+    }
+
+    public boolean eatAffordably(PaymentCard card) {
+        if (card.balance() >= 2.50) {
+            card.takeMoney(2.50);
+            this.affordableMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    public double eatHeartily(double payment) {
+        if (payment >= 4.30) {
+            this.money = this.money + 4.30;
+            this.heartyMeals++;
+            return payment - 4.30;
+        }
+        return payment;
+    }
+
+    public boolean eatHeartily(PaymentCard card) {
+        if (card.balance() >= 4.30) {
+            card.takeMoney(4.30);
+            this.heartyMeals++;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "money: " + money + ", number of sold affordable meals: " + affordableMeals
+                + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
